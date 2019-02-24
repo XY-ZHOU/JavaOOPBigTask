6dc85eb22622266798b58bdd85d9cd10dbfe279a
XY-ZHOU <731986251@qq.com>
2019-02-24 11:52:40 +0800

    override introduce function of student

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,14 +1,23 @@
 package main.java.model;
 
 public class Student extends Person {
-    public   Klass klass;
-    public Student(int id, String name, int age,Klass klass){
-        super(id,name,age);
-        this.klass=klass;
+    public Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
 
     }
+
     public Klass getKlass() {
         return klass;
+    }
+
+    public String introduce() {
+        if (klass.leader == null || !klass.leader.equals(this.name))
+            return (super.introduce() + " I am a Student. I am at Class " + getKlass().number);
+        else
+            return (super.introduce() + " I am a Student. I am a leader of " + getKlass().number);
     }
 
 }
