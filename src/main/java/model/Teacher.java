31eaa6600b0d2aa808daab487bcfe94369416148
XY-ZHOU <731986251@qq.com>
2019-02-24 12:01:45 +0800

    add isTeaching function of teacher

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,7 +1,5 @@
 package main.java.model;
 
-
-
 public class Teacher extends Person {
     public String[] klasses;
 
@@ -10,8 +8,22 @@ public class Teacher extends Person {
         this.klasses = klasses;
 
     }
+
     public String introduce() {
         String str = String.join(",", klasses);
         return str.length() == 0 ? super.introduce() + " I am a Teacher. I teach No Class." : super.introduce() + " I am a Teacher. I teach Class " + str + ".";
+    }
+
+    public String isTeaching(Student student) {
+        Klass klassOfStudent = student.getKlass();
+        if (klassOfStudent.isIn(klasses)) {
+            if ((student.name).equals(student.klass.leader)) {
+                return "I am " + name + ". I know " + student.name + " become Leader of Class " + student.klass.number + ".";
+            } else {
+                return "I am " + name + ". I know " + student.name + " has joined Class " + student.klass.number + ".";
+            }
+        } else {
+            return "";
+        }
     }
 }
