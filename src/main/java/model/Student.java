fd12b3d5e637757e7c55035928ff5b91386a7b02
XY-ZHOU <731986251@qq.com>
2019-02-24 11:48:37 +0800

    add property of student

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,12 @@
+package main.java.model;
+
+public class Student extends Person {
+    public   Klass klass;
+    public Student(int id, String name, int age,Klass klass){
+        super(id,name,age);
+        this.klass=klass;
+
+    }
+
+
+}
