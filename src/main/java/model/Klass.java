e1459fcc3cfbe6b88ba1081c9ecea525ecd02d27
XY-ZHOU <731986251@qq.com>
2019-02-24 11:48:22 +0800

    add property of klass

---
+++ src/main/java/model/Klass.java
@@ -0,0 +1,14 @@
+package main.java.model;
+
+
+
+public class Klass {
+    public String number;
+    public String leader;
+    public Klass(String number){
+        this.number=number;
+    }
+
+
+
+}
