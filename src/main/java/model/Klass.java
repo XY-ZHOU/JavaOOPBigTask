152dc2e04f844e8ab16e643c01ecf4eb9143c9f2
XY-ZHOU <731986251@qq.com>
2019-02-24 12:00:43 +0800

    add isIn function of klass

--- src/main/java/model/Klass.java
+++ src/main/java/model/Klass.java
@@ -1,24 +1,29 @@
 package main.java.model;
 
-
+import java.util.Arrays;
 
 public class Klass {
     public String number;
     public String leader;
-    public Klass(String number){
-        this.number=number;
+
+    public Klass(String number) {
+        this.number = number;
     }
-    public void assignLeader(Student student){
+
+    public void assignLeader(Student student) {
         this.leader = student.name;
     }
-    public String appendMember(Student student){
+
+    public String appendMember(Student student) {
         assignLeader(student);
-        if((student.klass.number).equals(number)){
+        if ((student.klass.number).equals(number)) {
             return "";
-        }else{
+        } else {
             return ("It is not one of us.");
         }
     }
 
-
+    public boolean isIn(String[] klasses) {
+        return (Arrays.asList(klasses).contains(number));
+    }
 }
