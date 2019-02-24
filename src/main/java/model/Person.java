08cf24980629e2fb85f0b0741defb2e0c4177652
XY-ZHOU <731986251@qq.com>
2019-02-24 11:44:45 +0800

    add property of person

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,5 +1,13 @@
 package main.java.model;
 
 public class Person {
+    public int id;
+    public String name;
+    public int age;
 
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
